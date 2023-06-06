c97f0d76d07cad82c9d28310b3b070e0ce961d04
tomasgomien <tomygomien@gmail.com>
2023-06-06 11:40:02 -0300

    Mejora visual y responsividad

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use('normalize',express.static(path.join(__dirname, '/node_modules/normalize.css')));
+
 app.get('/',function(req,res){
     res.sendFile((__dirname + '/views/home.html'))
 });
